ed782814b573cf93e8c491ab756822e8c6d34dfe
LaurEars <laura.rupprecht@server.fake>
2014-02-01 22:55:49 -0500

    Updated gauge image file name, fixed rgb encoding

--- gauge.py
+++ gauge.py
@@ -3,14 +3,14 @@ from PIL import Image
 import numpy as np
 from scipy.misc import imsave
 
-image_filename = 'gague-test-image.jpg'
-base_filename = 'gague'
+image_filename = 'oil_gauge.jpg'
+base_filename = image_filename.split('.')[0]
 my_image = pil.Image.open(image_filename).convert('L')
 np_image = np.array(pil.Image.open(image_filename))
-image_b = np_image[:,:,0]
-image_r = np_image[:,:,1]
-image_g = np_image[:,:,2]
-imsave(base_filename + '-g.pdf',image_g)
-imsave(base_filename + '-r.pdf',image_r)
-imsave(base_filename + '-b.pdf',image_b)
+image_r = np_image[:,:,0]
+image_g = np_image[:,:,1]
+image_b = np_image[:,:,2]
+imsave(base_filename + '-g.jpg',image_g)
+imsave(base_filename + '-r.jpg',image_r)
+imsave(base_filename + '-b.jpg',image_b)
 
